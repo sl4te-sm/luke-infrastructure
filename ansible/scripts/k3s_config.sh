5f06d6edca795e7a24135d62e3cc63a437f6f3e3
luke <lukemorton@tutanota.com>
2024-07-14 17:33:02 -0500

    fix: correct k3s config

--- ansible/scripts/k3s_config.sh
+++ ansible/scripts/k3s_config.sh
@@ -2,12 +2,12 @@
 
 INT_NAME="ens18"
 HOST_IP=$(ip addr show $INT_NAME | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-HOST_NAME=$(hostname -s)
+HOST_NAME=$(hostname)
 TOKEN=""
 AGENT_TOKEN=""
 
 # Exit if required files aren't present
-if ! [ -f /home/k3s/.ssl/${HOST_NAME}.cert.pem ] || ! [ -f /home/k3s/.ssl/${HOST_NAME}.key.pem ]; then
+if ! [ -f /home/k3s/.ssl/${HOST_NAME}-chain.cert.pem ] || ! [ -f /home/k3s/.ssl/${HOST_NAME}.key.pem ] || ! [ -f /home/k3s/.ssl/ca.cert.pem ]; then
   echo "Missing certificate files for etcd auth"
   exit 1
 elif [ -f /etc/rancher/k3s/k3s.yaml ]; then
@@ -19,8 +19,9 @@ fi
 curl -sfL https://get.k3s.io | sh -s - server \
   --node-taint CriticalAddonsOnly=true:NoExecute \
   --datastore-endpoint="https://luke-etcdtop-1.luke-domain.com:2379,https://luke-etcdtop-2.luke-domain.com:2379,https://luke-etcdtpo-3.luke-domain.com:2379" \
-  --datastore-cafile="/home/k3s/.ssl/${HOST_NAME}.cert.pem" \
+  --datastore-cafile="/home/k3s/.ssl/ca.cert.pem" \
   --datastore-keyfile="/home/k3s/.ssl/${HOST_NAME}.key.pem" \
+  --datastore-certfile="/home/k3s/.ssl/${HOST_NAME}-chain.cert.pem" \
   --token=${TOKEN} \
   --agent-token=${TOKEN} \
   --tls-san="${HOST_NAME}" \
