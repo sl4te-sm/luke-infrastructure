f7d4e7f5be1b8263406fab944d40314eb9e975d1
luke <lukemorton@tutanota.com>
2024-07-14 16:40:31 -0500

    fix: config dir was not found

---
+++ ansible/scripts/k3s_config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INT_NAME="ens18"
+HOST_IP=$(ip addr show $INT_NAME | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+HOST_NAME=$(hostname -s)
+TOKEN=""
+AGENT_TOKEN=""
+
+# Exit if required files aren't present
+if ! [ -f /home/k3s/.ssl/${HOST_NAME}.cert.pem ] || ! [ -f /home/k3s/.ssl/${HOST_NAME}.key.pem ]; then
+  echo "Missing certificate files for etcd auth"
+  exit 1
+elif [ -f /etc/rancher/k3s/k3s.yaml ]; then
+  echo "k3s already installed and configured"
+  exit 0
+fi
+
+# Install the server
+curl -sfL https://get.k3s.io | sh -s - server \
+  --node-taint CriticalAddonsOnly=true:NoExecute \
+  --datastore-endpoint="https://luke-etcdtop-1.luke-domain.com:2379,https://luke-etcdtop-2.luke-domain.com:2379,https://luke-etcdtpo-3.luke-domain.com:2379" \
+  --datastore-cafile="/home/k3s/.ssl/${HOST_NAME}.cert.pem" \
+  --datastore-keyfile="/home/k3s/.ssl/${HOST_NAME}.key.pem" \
+  --token=${TOKEN} \
+  --agent-token=${TOKEN} \
+  --tls-san="${HOST_NAME}" \
+  --tls-san="${HOST_IP}"
