9efdbe5eb757eb8b83a96d6ccc35cee8e35013b7
luke <lukemorton@tutanota.com>
2024-08-18 22:14:42 -0500

    feat: structured dpiproxy in linode

--- dpiproxy/singleServer/variables.tf
+++ dpiproxy/singleServer/variables.tf
@@ -1,25 +1,67 @@
+# Google Cloud variables
+variable gcpProject {
+  type = string
+  description = "Google Cloud Project name"
+  default = "dpiproxy"
+}
+
+variable gcpRegion {
+  type = string
+  description = "Google Cloud region"
+  default = "us-central1"
+}
+
+variable "gcpZone" {
+  type = string
+  description = "Google Cloud availability zone"
+  default = "us-central1-a"
+}
+
+variable gcsBucket {
+  type = string
+  description = "Google Cloud Storage Bucket name"
+  default = "dpiproxy-state-bucket"
+}
+
+# Linode variables
 variable apiKey {
   type = string
   sensitive = true
   description = "Linode API key"
 }
 
-variable gcpProject {
+variable linodeName {
   type = string
-  description = "Google Cloud Project name"
+  description = "Linode instance name"
+  default = "dpiproxy"
 }
 
-variable gcpRegion {
+variable linodeRegion {
   type = string
-  description = "Google Cloud region"
+  description = "Linode instance region"
+  default = "us-ord"
 }
 
-variable "gcpZone" {
+variable linodeImage {
   type = string
-  description = "Google Cloud availability zone"
+  description = "Linode instance image"
+  default = "linode/almalinux9"
 }
 
-variable gcsBucket {
+variable linodeType {
   type = string
-  description = "Google Cloud Storage Bucket name"
+  description = "Linode instance type"
+  default = "g6-nanode-1"
+}
+
+variable linodeSSHKeys {
+  type = list(string)
+  description = "Linode instance SSH keys"
+  sensitive = true
+}
+
+variable linodePassword {
+  type = string
+  description = "Linode instance root password"
+  sensitive = true
 }
