f257a39cf66f945e969cc450570700ba12dceb63
sl4te-sm <codesign@mail.luke-domain.com>
2025-08-16 09:42:31 -0500

    feat: deploy cilium BGP

--- apps/cilium/template.sh
+++ apps/cilium/template.sh
@@ -3,7 +3,7 @@
 helm template \
     cilium \
     cilium/cilium \
-    --version 1.17.3 \
+    --version 1.18.0 \
     --namespace kube-system \
     --set ipam.mode=kubernetes \
     --set kubeProxyReplacement=true \
@@ -16,7 +16,7 @@ helm template \
     --set gatewayAPI.enabled=true \
     --set gatewayAPI.enableAlpn=true \
     --set gatewayAPI.enableAppProtocol=true \
-    --set gatewayAPI.hostNetwork.enabled=true \
     --set envoy.securityContext.capabilities.keepCapNetBindService=true \
-    --set envoy.securityContext.capabilities.envoy="{NET_BIND_SERVICE,NET_ADMIN,PERFMON,BPF}"\
-    --set k8sServicePort=7445 > cilium2.yaml
+    --set envoy.securityContext.capabilities.envoy="{NET_BIND_SERVICE,NET_ADMIN,PERFMON,BPF}" \
+    --set bgpControlPlane.enabled=true \
+    --set k8sServicePort=7445 >cilium2.yaml
